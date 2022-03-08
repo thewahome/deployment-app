b0e99d5e115f53321f652777c3938fb8546e26a4
Charles Wahome <thewahome.cw@gmail.com>
2022-03-08 11:42:52 +0300

    preprocess html with script tags

--- scripts/create-html.ps1
+++ scripts/create-html.ps1
@@ -1,25 +1,15 @@
-#=== Copy file from original location === 
-$path = "../public/index.html"
-$srcFile = ".\index.html"  # source file that has the insert marker  
-Copy-Item -Path $path -Destination $srcFile
+$srcFile = ".\index.html"  # source file that has the insert marker 
+
+#=== Fetch file from the set location === 
+Invoke-WebRequest https://gewayhome.z13.web.core.windows.net | 
+Select-Object -ExpandProperty Content | 
+Out-File $srcFile 
 
 #=== Add script lines to file === 
 $scriptTags = 
 '<script defer="defer" src="/static/js/main.3d6d2a62.js"></script>
   <link href="/static/css/main.073c9b0a.css" rel="stylesheet">'
-$scriptTags | Out-File -FilePath '.\content.txt'
+$scriptTags += '</head>'
 
-#=== Write the index file to the output file === 
-$textToInsert = '.\content.txt'
-$srcFile = ".\index.html"  # source file that has the insert marker  
-$dstFile = ".\index2.html"  # destination file for combined input
- 
-$pattern = [regex]'<title>'
-$lineNum = (Select-String -Pattern $pattern -Path $srcFile).lineNumber
-Write-Verbose "Inserting at line number:  $lineNum"
- 
-Invoke-Command { 
-  Get-Content -Path $srcFile | select -First $lineNum
-  Get-Content -Path $textToInsert
-  Get-Content -Path $srcFile | select -Skip $lineNum
-} | Set-Content -Path $dstFile -Force
+(Get-Content $srcFile) -replace '</head>', $scriptTags | 
+	Set-Content $srcFile
