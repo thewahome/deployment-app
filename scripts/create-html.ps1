bbe03e4805c8ea48e72a25b6ca55665d356224ab
Charles Wahome <thewahome.cw@gmail.com>
2022-03-08 14:34:27 +0300

    republish artifacts

--- scripts/create-html.ps1
+++ scripts/create-html.ps1
@@ -1,17 +1,20 @@
-#=== Source script line from build file === 
+$url = "$($URL)"
+Write-Host "URL: $url"
+$htmlContent = Invoke-WebRequest $url | Select-Object -ExpandProperty Content
+Write-Host "Pipeline = $($htmlContent)"
+
 $buildFile = '../public/index.html'
 $content = Get-Content -Path $buildFile -Raw 
 $html = New-Object -ComObject "HTMLFile"
 $html.IHTMLDocument2_write($content)
 $scripts = $html.all.tags('script') | ForEach-Object src
 
-
-#=== Generate script lines === 
 $scriptTags = ''
 foreach ($source in $scripts) {
   $scriptTags += "<script src='$source'></script>"
 }
 $scriptTags += '</head>'
 
-(Get-Content $buildFile) -replace '</head>', $scriptTags | 
-	Set-Content $buildFile
+$htmlContent -replace '</head>', $scriptTags | 
+Set-Content $buildFile -Force
+$htmlContent
