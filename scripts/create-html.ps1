9a65c8cfd19a3ec59607cd56e53540f5875d2e3a
Charles Wahome <thewahome.cw@gmail.com>
2022-03-16 16:40:49 +0300

    add css links

--- scripts/create-html.ps1
+++ scripts/create-html.ps1
@@ -1,20 +1,28 @@
-$url = "$($URL)"
+$url = "https://gewayhome.z13.web.core.windows.net/cdf4e7f84c05e9f757dab037bc354d22739cc09d"
 Write-Host "URL: $url"
 $htmlContent = Invoke-WebRequest $url | Select-Object -ExpandProperty Content
-Write-Host "Pipeline = $($htmlContent)"
 
-$buildFile = '../public/index.html'
+$buildFile = '../build/index.html'
 $content = Get-Content -Path $buildFile -Raw 
 $html = New-Object -ComObject "HTMLFile"
 $html.IHTMLDocument2_write($content)
+
 $scripts = $html.all.tags('script') | ForEach-Object src
+$cssLinks = $html.all.tags('link') |
+Where-Object { $_.rel -eq 'stylesheet' } |
+Select-Object -Expand href
 
 $scriptTags = ''
 foreach ($source in $scripts) {
-  $scriptTags += "<script src='$source'></script>"
+  $scriptTags += "<script src='$source' defer></script>"
+}
+
+foreach ($link in $cssLinks) {
+  $scriptTags += "<link rel='stylesheet' href='$link'>"
 }
 $scriptTags += '</head>'
 
+Write-Host "Tags found: = $($scriptTags)"
+
 $htmlContent -replace '</head>', $scriptTags | 
 Set-Content $buildFile -Force
-$htmlContent
